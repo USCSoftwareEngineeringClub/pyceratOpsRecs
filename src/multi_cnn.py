ef6e22c8a946d508da5e167ba51e77e69838c823
tbhall <tbhall@email.sc.edu>
2016-10-20 01:59:34 -0400

    Resource Exhaustion fixed as well as improved training accuracy

--- src/multi_cnn.py
+++ src/multi_cnn.py
@@ -13,36 +13,35 @@
 # limitations under the License.
 # ==============================================================================
 
+# This code has been modifed by Tyler Hall (very minimally but it has been modifed)
+
+#Imports
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-# Import data
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 import tensorflow as tf
+sess = tf.InteractiveSession()
 
-
-flags = tf.app.flags
-FLAGS = flags.FLAGS
-flags.DEFINE_string('data_dir', '/tmp/data/', 'Directory for storing data')
-
-# Placeholders
+#Place Holder
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-#Variables
 W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
 
-sess = tf.InteractiveSession()
+y = tf.nn.softmax(tf.matmul(x,W) + b)
 
-y = tf.matmul(x,W) + b
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
-
-# Weight & Bias Initialization Functions
-
+"""
+    Functions for
+    Weight
+    Bias
+    Convolution
+    Pooling
+"""
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
@@ -51,23 +50,25 @@ def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
-# Convolution and Pooling Functions
+
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1],padding='SAME')
 
-#First Convolutional Layer
+
+# First Layer
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
+
 
 x_image = tf.reshape(x, [-1,28,28,1])
 
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
-#Second Convolutional Layer
+#Second Layer
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
@@ -91,20 +92,28 @@ b_fc2 = bias_variable([10])
 
 y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-#Train and Evaluate the Model
+#Training && Test
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.initialize_all_variables())
+
+init = tf.initialize_all_variables()
+config = tf.ConfigProto()
+config.gpu_options.allocator_type = 'BFC'
+with tf.Session(config = config) as s:
+    sess.run(init)
+
 for i in range(20000):
     batch = mnist.train.next_batch(50)
     if i%100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1],keep_prob: 1.0})
+        train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
         print("step %d, training accuracy %g"%(i, train_accuracy))
-        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+for i in xrange(10):
+    testSet = mnist.test.next_batch(50)
+    print("test accuracy %g"%accuracy.eval(feed_dict={ x: testSet[0], y_: testSet[1], keep_prob: 1.0}))
 
 
 
