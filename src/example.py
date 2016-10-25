c24bb5fb7556eae58edb38a4b0d654eca0209e79
myersr <rm2@email.sc.edu>
2016-10-25 15:12:13 -0400

    I have the api set up and a url hardcoded into the request for some reason I get a JSON format error back from microsoft. I am at a loss on why. Anyone feel free to tke a look

---
+++ src/example.py
@@ -0,0 +1,50 @@
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '{subscription key}',
+}
+
+params = urllib.urlencode({
+    # Request parameters
+    'language': 'unk',
+    'detectOrientation ': 'true',
+})
+
+try:
+    conn = httplib.HTTPSConnection('api.projectoxford.ai')
+    conn.request("POST", "/vision/v1.0/ocr?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
+
+########### Python 3.2 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '{subscription key}',
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters
+    'language': 'unk',
+    'detectOrientation ': 'true',
+})
+
+try:
+    conn = http.client.HTTPSConnection('api.projectoxford.ai')
+    conn.request("POST", "/vision/v1.0/ocr?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
--- src/interface/hello.py
+++ src/interface/hello.py
@@ -1,38 +1,54 @@
-import httplib, urllib, base64
-from flask import Flask, render_template, request
+import requests, urllib, httplib, base64
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
-@app.route("/search/", methods=['POST', 'GET'])
+@app.route("/search", methods=['POST', 'GET'])
 def callAPI():
 	error = None
-	_url = 'https://api.projectoxford.ai/vision/v1/ocr' #https://api.projectoxford.ai/vision/v1.0/ocr[?language][&detectOrientation ]
+	_url = 'https://api.projectoxford.ai/vision/v1.0/ocr' #https://api.projectoxford.ai/vision/v1.0/ocr[?language][&detectOrientation ]
 	_key = 'f8968ffd96d2475cb7ec347c51f24e3e' #Here you have to paste your primary key it is a header
 	_maxNumRetries = 10
 
-	headers = {
-	    # Request headers
+	bodyURL = request.args.get('uri','')
+	print(bodyURL)
+
+	headersIn = {
 	    'Content-Type': 'application/json',
 	    'Ocp-Apim-Subscription-Key': _key,
 	}
 
-	params = urllib.urlencode({
-	    # Request parameters
-	    'detectOrientation ': 'true',
+	paramsIn = urllib.urlencode({
+		'language': 'en',
+	    'detectOrientation': 'false'
 	})
 
 	try:
-	    conn = httplib.HTTPSConnection('api.projectoxford.ai')
-	    conn.request("POST", "/vision/v1.0/ocr?%s" % params, "{body}", headers)
-	    response = conn.getresponse()
-	    data = response.read()
-	    print(data)
-	    conn.close()
+		r = requests.post(_url, data={"url":"https://csgsarchitects.files.wordpress.com/2011/12/111_new-blog.jpg"},\
+	    			 params=paramsIn, headers=headersIn)
+		print r.content
+		print r.json()
+		return r.text
+	    #
+		# conn = httplib.HTTPSConnection('api.projectoxford.ai')
+		# conn.request("POST", "/vision/v1.0/ocr?%s" % paramsIn, "{body}", headersIn)
+		# response = conn.getresponse()
+		# data = response.read()
+		# print(data)
+		# conn.close()
+		#
+	    # print 'hello'
+	    # conn.request("POST", "/vision/v1.0/ocr?%s" % params, {"url":"http://example.com/images/test.jpg"}, headers)
+	    # response = conn.getresponse()
+	    # data = response.read()
+	    # print(data)
+	    # conn.close()
 	except Exception as e:
-	    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+	    print(e)
+	    return e
 
 
 
