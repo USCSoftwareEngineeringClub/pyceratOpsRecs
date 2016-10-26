9029d42998dc0e15b5d71028adc8c0a8bb1742e7
EduardoRomero83 <earg8395@hotmail.com>
2016-10-26 19:44:18 -0400

    Fixed indentation of AdaBoost

--- src/models/AdaBoost.py
+++ src/models/AdaBoost.py
@@ -1,9 +1,7 @@
 class AdaBoost(OCRModel):
 	"""Ada Bosts collects models to present a collective model"""
 
-	def __init__(self, models):
-		self.models = models
-
+    def __init__(self, models):
+        self.models = models
     def train(self, tests, ans): pass
-
     def run(self, test): pass
